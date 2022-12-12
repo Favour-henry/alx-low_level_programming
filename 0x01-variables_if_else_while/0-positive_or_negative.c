6ac0954a62e6427366f4fbab9ab1df8ded0cfad7
Favour-henry <nnakeefavour 2022@gmail.com>
2022-12-11 20:50:55 -0800

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 
 /* betty style doc for function main goes there */
-/***
+/**
  * main - Entry point
  *
  * Return: Always 0 (success)
@@ -13,7 +13,6 @@
 int main(void)
 {
 	int n;
-
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
