345011bd2c946ee397baf6ec38438270eba95522
Favour-henry <nnakeefavour 2022@gmail.com>
2022-12-14 19:46:15 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,11 @@
 
 #include "main.h"
 
-
-
 /**
+ * print_alphabet - prints the alphabet
  *
- *  * print_alphabet - prints the alphabet
- *
- *   * Return: 0 (Success)
- *
- *    */
-
-
+ * Return: 0 (Success)
+ */
 
 void print_alphabet(void)
 
@@ -19,26 +13,13 @@ void print_alphabet(void)
 
 	char x;
 
-
-
 	x = 'a';
-
-
 
 	while (x <= 'z')
 
 	{
-
 		_putchar(x);
-
 		x++;
-
 	}
-
-
-
 	_putchar('\n');
-
-
-
 }
