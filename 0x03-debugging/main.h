a02fc2699ae519acdead115c4064aec727718985
Favour-henry <nnakeefavour 2022@gmail.com>
2022-12-15 19:41:12 -0800

    task 0 debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,5 +6,6 @@ void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int print_remaining_days(int month, int day, int year);
 int convert day(int month, int day);
+int print_times_table(int n);
 
 #endif
