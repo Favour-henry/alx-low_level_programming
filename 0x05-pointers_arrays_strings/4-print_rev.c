cfe2dee9a5d654905b64ac399e0e279c2cd95956
Favour-henry <nnakeefavour 2022@gmail.com>
2022-12-19 08:50:30 -0800

    task 234

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * primt_rev - prints a reverse strings
+ * @s: this is input string
+ */
+void print_rev(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; index++)
+		;
+	for (index = index = - 1; s[index] != '\0'; index--)
+	{
+		_putchar(s[index]);
+	}
+	_putchar('\n');
+}
