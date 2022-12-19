434349970c7686eca0481c037b36bd5ccc11361c
Favour-henry <nnakeefavour 2022@gmail.com>
2022-12-19 09:45:56 -0800

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _puts - writes a function that prints a string, followed by a new line, to stdout
+ * _puts - writes a function that prints a string, followed by a new line, to
+ * stdout.
  * @str: this is input string
  */
 
