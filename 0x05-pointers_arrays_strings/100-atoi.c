a3902ab588efa684b100494318285897bbebb064
Favour-henry <nnakeefavour 2022@gmail.com>
2022-12-19 23:28:34 -0800

    task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -24,7 +24,7 @@ int _atoi(char *s)
 		do {
 			num = num * 10 + (*temp - '0');
 			temp++;
-		} while (*temp >= '0' && *temp is <= '9');
+		} while (*temp >= '0' && *temp <= '9');
 	}
-	return (num * sign):
+	return (num * sign);
 }
