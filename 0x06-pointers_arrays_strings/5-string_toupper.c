2aa6b7832fc0e9b0bfa3b3a399eefd07797d7843
Favour-henry <nnakeefavour 2022@gmail.com>
2022-12-22 07:20:20 -0800

    tasks

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercases in a string to uppercase
+ * @str: parameter
+ * Returns: returns a character
+ */
+char *string_toupper(char *)
+{
+	int index = 0;
+
+	while (str[index++])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+	}
+	return (str);
+}
