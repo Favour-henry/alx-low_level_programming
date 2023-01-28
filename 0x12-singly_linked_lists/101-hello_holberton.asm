5118be8b4e6c9fce30fc39b7338839023613f956
Favour-henry <nnakeefavour2022@gmail'com>
2023-01-27 20:20:55 -0800

    Linked and printf

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,21 @@
+extern printf
+
+section .text
+   global main
+   
+main;
+   push rpb
+   
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0
+   call printf
+   
+   pop rpb
+   
+   mov rax,0
+   ret
+
+section.data
+msg: do "Hello,Holberton",0
+fmt: do "%s", 10, 0
